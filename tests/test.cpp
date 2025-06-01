010f855edd0b749de68e4c3011cd20c1bef0c46b
Walcheri <vagzvrf@mail.ru>
2025-06-01 22:25:18 +0300

    done

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,6 +4,11 @@
 #include "Transaction.h"
 #include <gmock/gmock.h>
 
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::Ref;
+
 class MockAccount : public Account {
 public:
     MockAccount(int id, int balance) : Account(id, balance) {}
@@ -22,16 +27,16 @@ public:
 TEST(AccountTest, MockMethodsTest) {
     MockAccount account(1, 1000);
     
-    EXPECT_CALL(account, GetBalance()).Times(testing::AtLeast(1));
-    account.GetBalance();
+    EXPECT_CALL(account, GetBalance()).WillOnce(Return(1000));
+    EXPECT_EQ(1000, account.GetBalance());
     
-    EXPECT_CALL(account, Lock()).Times(testing::AtLeast(1));
+    EXPECT_CALL(account, Lock()).Times(1);
     account.Lock();
     
-    EXPECT_CALL(account, ChangeBalance(1)).Times(testing::AtLeast(1));
-    account.ChangeBalance(1);
+    EXPECT_CALL(account, ChangeBalance(100)).Times(1);
+    account.ChangeBalance(100);
     
-    EXPECT_CALL(account, Unlock()).Times(testing::AtLeast(1));
+    EXPECT_CALL(account, Unlock()).Times(1);
     account.Unlock();
 }
 
@@ -40,8 +45,8 @@ TEST(TransactionTest, MockSaveToDatabaseTest) {
     MockAccount to(2, 10000);
     MockTransaction transaction;
     
-    EXPECT_CALL(transaction, SaveToDataBase(testing::_, testing::_, 1999))
-        .Times(testing::AtLeast(1));
+    EXPECT_CALL(transaction, SaveToDataBase(Ref(from), Ref(to), 1999))
+        .Times(1);
     transaction.Make(from, to, 1999);
 }
 
@@ -63,17 +68,18 @@ TEST(AccountTest, RealMethodsBehavior) {
 TEST(TransactionTest, RealMethodsBehavior) {
     Account acc1(1, 10000);
     Account acc2(2, 10000);
-    Transaction defaultFeeTransaction;
-    Transaction customFeeTransaction;
-    customFeeTransaction.set_fee(500);
+    Transaction t;
     
-    EXPECT_THROW(defaultFeeTransaction.Make(acc1, acc1, 100), std::logic_error);
-    EXPECT_THROW(defaultFeeTransaction.Make(acc1, acc2, -100), std::invalid_argument);
-    EXPECT_THROW(defaultFeeTransaction.Make(acc1, acc2, 0), std::logic_error);
+    // Проверка исключений
+    EXPECT_THROW(t.Make(acc1, acc1, 100), std::logic_error);
+    EXPECT_THROW(t.Make(acc1, acc2, -100), std::invalid_argument);
+    EXPECT_THROW(t.Make(acc1, acc2, 0), std::invalid_argument);
     
-    EXPECT_FALSE(customFeeTransaction.Make(acc1, acc2, 200));
+    // Проверка успешного перевода
+    EXPECT_TRUE(t.Make(acc1, acc2, 1000));
+    EXPECT_EQ(9000, acc1.GetBalance());
+    EXPECT_EQ(11000, acc2.GetBalance());
     
-    defaultFeeTransaction.Make(acc1, acc2, 1999);
-    EXPECT_EQ(8000, acc1.GetBalance());
-    EXPECT_EQ(11999, acc2.GetBalance());
+    // Проверка недостаточного баланса
+    EXPECT_FALSE(t.Make(acc1, acc2, 10000));
 }
